b6a523327580ec1297e34af9b2d59085ce0b871a
Rithik Pradhan <pradhanrithik62@gmail.com>
2026-01-27 18:31:58 +0530

    Link added in app.ts file

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "https://forum-appfrontend.vercel.app",
+    ],
     credentials: true,
   }),
 );
